0dbfb9da9e7ea5463b091bc016f7d9fb1983c0dc
mfaizadya <m.faiz.adya@gmail.com>
2025-03-09 14:36:05 +0700

    menambahkan implementasi dengan state-based

--- Program.cs
+++ Program.cs
@@ -31,7 +31,51 @@ class KodePos
         }
     }
 }
-class Program
+
+class DoorMachine
+{
+    private enum State {Terkunci, Terbuka}
+    private State currentState;
+
+    public DoorMachine()
+    {
+        currentState = State.Terkunci; // State awal terkunci
+    }
+
+    public void KunciPintu()
+    {
+        if (currentState == State.Terbuka)
+        {
+            currentState = State.Terkunci;
+            Console.WriteLine("Pintu terkunci.");
+        }
+        else
+        {
+            Console.WriteLine("Pintu sudah dalam keadaan terkunci.");
+        }
+    }
+
+    public void BukaPintu()
+    {
+        if (currentState == State.Terkunci)
+        {
+            currentState = State.Terbuka;
+            Console.WriteLine("Pintu tidak terkunci.");
+        }
+        else
+        {
+            Console.WriteLine("Pintu sudah terbuka.");
+        }
+    }
+
+    public void TampilkanStatus()
+    {
+        Console.WriteLine($"Status pintu saat ini: {currentState}");
+    }
+
+}
+
+    class Program
 {
     static void Main()
     {
@@ -43,6 +87,15 @@ class Program
         {
             Console.WriteLine($"{kelurahan}: {kodePos.getKodePos(kelurahan)}");
         }
+
+        DoorMachine pintu = new DoorMachine();
+
+        pintu.TampilkanStatus();
+        pintu.BukaPintu();
+        pintu.TampilkanStatus();
+        pintu.KunciPintu();
+        pintu.TampilkanStatus();
+
     }
 }
 
